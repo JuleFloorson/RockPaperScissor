bb7c8e7c8f4b235721e29d829ef95d50213490c8
JuleFloorson <jubo93@web.de>
2020-03-13 17:40:28 +0100

    Add router home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="../pages/home">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+
+        <Footer />
+      </Router>
     </>
   );
 }
