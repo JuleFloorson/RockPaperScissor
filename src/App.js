b7574580b7b7c9a296b16b2b08639fb163a39d7e
JuleFloorson <jubo93@web.de>
2020-03-13 16:56:10 +0100

    Correct flexbox and add files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header></Header>
-
+      <Home></Home>
       <Footer></Footer>
     </>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import './Home.css';
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <button className="CPU_button">1 vs CPU</button>
+      <button className="twoPlayer_button">1 vs 1</button>
+    </>
+  );
+}
+export default Home;
